d1a5c8ca1272cf6b24af1ae4efa2c9490efee648
tanazimmer <68484974+tanazimmer@users.noreply.github.com>
2020-11-15 20:56:15 -0500

    read into data

---
+++ app.js
@@ -0,0 +1,60 @@
+// Step 1: Set up our chart
+//= ================================
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 50
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Select body, append SVG area to it, and set the dimensions
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Load data from data.csv
+d3.csv("data.csv").then(function(smokerData) {
+
+    // Format data
+    smokerData.forEach(function(data) {
+    data.smokes = +data.smokes;
+    data.age = +data.age;
+  });
+
+  
+  // Create Scales
+    var xScale = d3.scaleLinear()
+    .domain(d3.extent(smokerData, d => d.smokes))
+    .range([0, width]);
+
+  var yScale = d3.scaleLinear()
+    .domain([0, d3.max(smokerData, d => d.age)])
+    .range([height, 0]);
+
+  // //create Axes
+
+    var bottomAxis = d3.axisBottom(xScale);
+    var leftAxis = d3.axisLeft(yScale);
+
+    
+  // Append the axes to the chartGroup - ADD STYLING
+  // Add bottomAxis
+  chartGroup.append("g")
+  .attr("transform", `translate(0, ${height})`)
+  .call(bottomAxis);
+
+  
+}).catch(function(error) {
+  console.log(error);
+});
